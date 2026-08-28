b99da46db4cd74d89d227c644dae5e2057b25a5f
aysha sahva <ayshasahva@gmail.com>
2026-08-28 22:03:05 +0530

    feat: add request logger middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from 'cors'
-import errorHandler from "./middleware/error.middleware";
+import errorHandler from "./middleware/error.middleware.js";
+import router from "./modules/auth/auth.routes.js";
+import logger from "./middleware/logger.middleware.js";
 
 const app = express();
 
@@ -8,9 +10,11 @@ app.use(express.json());
 app.use(cors())
 
 app.get("/", (req, res) => {
-  res.send("tinynest API is running successfully");
+    res.send("tinynest API is running successfully");
 });
+app.use(logger);
 
+app.use("/api/auth", router);
 app.use((req, res) => {
     res.status(404).json({
         success: false,
