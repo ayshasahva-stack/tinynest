b8f008a82d1e8995f2a6413d6408e2ed1c23e3b5
aysha sahva <ayshasahva@gmail.com>
2026-08-28 21:42:28 +0530

    feat: add cors middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,13 @@
-import express from 'express'
-const app = express()
+import express from "express";
+import cors from 'cors'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
 app.get("/", (req, res) => {
-    res.send("tinynest running")
-})
-export default app
+  res.send("tinynest API is running successfully");
+});
+
+export default app;
